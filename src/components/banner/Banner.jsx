c67b8f9ac2ea0bd463aba497c61bd998cb82986b
masud-jatri <masud.jatri@gmail.com>
2022-02-20 18:38:20 +0600

    banner component added

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+import banner from '../../asset/banner.png'
+
+const Banner = () => {
+    return (
+        <div style={{backgroundImage:`url(${banner})`}}>
+
+        </div>
+    );
+};
+
+export default Banner;
